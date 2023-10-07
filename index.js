3563de3d691299bc316ae4ae0c2373dc8b7c716b
taliieva <teranealieva01@gmail.com>
2023-10-07 21:55:34 +0400

    third commit

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ const screen = document.querySelector(".screen");
 const buttons = document.querySelectorAll("button");
 
 let output = "";
+screen.value = "";
 
 
 buttons.forEach((button)=>{
     button.addEventListener("click",(event)=>{
-        if(event.target.innerText == "="){
+        if(event.target.innerText === "=" && screen.value.length === 0){
+            screen.value = "";
+        }
+        else if(event.target.innerText == "="){
             output = String(eval(output))
             screen.value = output;
         }
-        else if(event.target.innerText == "AC"){
+        
+        else if(event.target.innerText === "AC"){
             output = ""
             screen.value = output;
         }
-        else if(event.target.innerText == "+/-"){
+        else if(event.target.innerText === "+/-"){
             output = String(-eval(output))
             screen.value = output;
         }
-        else if(event.target.innerText == "%"){
+        else if(event.target.innerText === "%"){
             output = eval(output/100)
             screen.value = output;
         }
